2db19b635ef0db43c5d0d2111dbd1bdd3e95dcc2
Amanda Crawford <crawforda0502@gmail.com>
2024-01-11 15:35:31 -0600

    hope this works!

---
+++ booleans-and-conditionals/practice/boolean-practice.js
@@ -0,0 +1,102 @@
+/* console.log(Boolean("true"));
+console.log(Boolean("TRUE"));
+console.log(Boolean("FALSE"));
+console.log(Boolean(1));
+console.log(Boolean('LaunchCode'));
+console.log(5 == 5);
+console.log(7 == "7");
+console.log(0 == false);
+console.log(0 == '');
+
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(5 == 6);
+console.log(true == "true");
+
+console.log(typeof (3 == 4));
+console.log(typeof (3 + 4));
+console.log(typeof (3 + 4 === 7));
+console.log(typeof "false"); */
+
+/* console.log(7 == "7");
+console.log(0 == false);
+console.log(0 == '');
+console.log((3 > 1) == true);
+console.log((3 > 1) == false);
+console.log((3 > 1) == "false");
+
+console.log('0' == 0);
+console.log(0 == '');
+console.log('0' == '');
+
+console.log(7 === "7");
+console.log(0 === false);
+console.log(0 === '');
+
+console.log(4 == "4"); */
+
+/* console.log(7 > 5 && 5 > 3);
+console.log(7 > 5 && 2 > 3);
+console.log(2 > 3 && 'dog' === 'cat');
+console.log(7 > 5 || 5 > 3);
+console.log(7 > 5 || 2 > 3);
+console.log(2 > 3 || 'dog' === 'cat');
+console.log(!true);
+console.log(!false);
+console.log( !(5 > 7) );
+console.log( !('dog' === 'cat') ); */
+
+// OOO: !, exp (**), arithmetic (*/,+-), comparison (>=<=<>,===!==) AND (&&), OR (||)
+// ((x * 5) >= 10) && ((y - 6) <= 20)
+// ((5*3) > 10) && ((4 + 6) === 11)
+// console.log(4 < 3 || 2 < 3);
+
+/* let billHasBeenPaid = true;
+
+if (!billHasBeenPaid) {
+   console.log("Your bill is due soon!");
+} else {
+   console.log("Your payments are up to date.");
+}
+
+let x = 10;
+let y = 20;
+
+if (x > y) {
+   console.log("x is greater than y");
+} else if (x < y) {
+   console.log("x is less than y");
+} else if (x % 5 === 0) {
+   console.log("x is divisible by 5");
+} else if (x % 2 === 0) {
+   console.log("x is even");
+} */
+
+/* let a = 7;
+if (a % 2 === 1) {
+   console.log("Launch");
+} else if (a > 5) {
+   console.log("Code");
+} else {
+   console.log("LaunchCode");
+}
+*/
+
+/* let num = -8;
+
+if (num % 2 === 0) {
+    console.log("EVEN");
+
+    if (num > 0) {
+        console.log("POSITIVE");
+    }
+}
+*/
+
+let num = 7;
+
+if (num % 2 === 0) {
+    if (num % 2 === 1) {
+        console.log("odd");
+    }
+}
