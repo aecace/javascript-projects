e2d750b3183584086b7a3f44d28162d6383b9ca3
Amanda Crawford <crawforda0502@gmail.com>
2024-01-22 11:56:21 -0600

    Class 4 reading practice

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,9 @@
 // create an array variable containing the names
 
+let names = ["Anna", "Bob", "Charles", "Dwayne"];
+
 // write a for loop that prints each name on a different line
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
--- loops/chapter-examples/while-Loop-Example.js
+++ loops/chapter-examples/while-Loop-Example.js
@@ -1,6 +1,23 @@
+// let i = 0;
+
+// while (i < 51) {
+//   console.log(i);
+//   i++;
+// }
+
+let numbers = [18, 59, -7, 5];
+let searchVal = 42;
 let i = 0;
 
-while (i < 51) {
-  console.log(i);
-  i++;
+while (i < numbers.length) {
+   if (numbers[i] === searchVal) {
+      break;
+   }
+   i++;
+}
+
+if (i < numbers.length) {
+   console.log("The value", searchVal, "was located at index", i);
+} else {
+   console.log("The value", searchVal, "is not in the array.");
 }
