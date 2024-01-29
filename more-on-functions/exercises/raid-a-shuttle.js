792f9474418efce023fb0cccc7ad3696b80fd370
Amanda Crawford <crawforda0502@gmail.com>
2024-01-29 15:37:46 -0600

    Class 6 Exercise

--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -24,10 +24,21 @@ let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold
 console.log("Fuel level: " + checkFuel(fuelLevel));
 console.log("Hold status: " + holdStatus(cargoHold));
 
-/* Steal some fuel from the shuttle:
- * /
- 
+// Steal some fuel from the shuttle:
+
 //a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
+
+let anonFunction = function(input) {
+  if (checkFuel(input) === 'green') {
+     return input - 100001;
+  }
+  else if (checkFuel(input) === 'yellow') {
+     return input - 50001;
+  }
+  else {
+     return input;
+  }
+};
 
 //b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
 
@@ -35,8 +46,15 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 //d). Decide where to best place your function call to gather our new fuel.
 
-/* Next, liberate some of that glorious cargo.
- * /
+// Next, liberate some of that glorious cargo.
+
+let anonFunction2 = function(input2) {
+  let arr2 = cargoHold.splice(0, 2, "a", "a");
+  return arr2;
+}
+
+let emptyArr = [];
+//console.log(anonFunction2(emptyArr));
 
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
 
@@ -46,9 +64,14 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 //d). Don’t get hasty, matey! Remember to test your function.
 
-/* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
- * /
- 
+// Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
+
+let irs = function(levelOfFuel, itemsInCargo) {
+  let arr = anonFunction2(itemsInCargo);
+  return `Raided ${anonFunction(fuelLevel)} kg of fuel from the tanks, and stole ${arr[0]} and ${arr[1]} from the cargo hold.`
+}
+
+console.log(irs(10000, 7));
 //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
 	
 //b). Call your anonymous fuel and cargo functions from within irs.
