55d068190921a09b4d345e37c6699e92c443cd44
Amanda Crawford <crawforda0502@gmail.com>
2024-02-05 13:03:18 -0600

    Class 8 Exercises + Practice done

--- modules/exporting-modules/practiceExports.js
+++ modules/exporting-modules/practiceExports.js
@@ -14,3 +14,11 @@ function randomArrayElement(arr){
     let index = Math.floor(Math.random()*arr.length);
     return arr[index];
 }
+
+//module.exports = isPalindrome;
+//Node expects only ONE module.exprts statements in a file
+module.exports = {
+    isPalindrome: isPalindrome,
+    evenOrOdd: evenOrOdd,
+    randomArrayElement: randomArrayElement
+}
