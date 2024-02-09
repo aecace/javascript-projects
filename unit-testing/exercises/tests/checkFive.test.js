0a36b4a0418911bbbfaefc5b747ea7cfe7521c7c
Amanda Crawford <crawforda0502@gmail.com>
2024-02-08 19:45:55 -0600

    Minor Updates

--- unit-testing/exercises/tests/checkFive.test.js
+++ unit-testing/exercises/tests/checkFive.test.js
@@ -1,10 +1,20 @@
-const checkFive = require('../checkFive.js');
+const checkFive = require('../checkFive');
 
 describe("checkFive", function(){
 
-    test("returns 'num is less than 5' when num < 5.", function() {
+   it("returns 'num is less than 5' when num < 5.", function() {
+      let output = checkFive(5);
+      expect(output).toEqual("5 is equal to 5.");
+   });
+
+   it("returns 'num is less than 5' when num < 5.", function() {
       let output = checkFive(2);
       expect(output).toEqual("2 is less than 5.");
+   });
+
+   it("returns 'num is less than 5' when num < 5.", function() {
+      let output = checkFive(15);
+      expect(output).toEqual("15 is greater than 5.");
    });
 
 });
--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,32 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployee: 100,
+  programOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(number) {
+    let output = '';
+    if (num % 2 === 0) {
+        output += 'Launch';
+    }
+    if (num % 3 === 0) {
+        output += 'Code';
+    }
+    if (num % 5 === 0) {
+        if (output) {
+            output += 'Rocks';
+        } else {
+            output += 'Rocks';
+        }
+    }
+    if (!output) {
+        return `Rutabagas! That doesn't work.`;
+    } else if (output === 'Launch Rocks') {
+        return output + ' ! Smack!!!!';
+    } else {
+        return output + '!';
+    }
+  }
 }
 
 module.exports = launchcode;
